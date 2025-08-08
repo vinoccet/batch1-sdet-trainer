8aa2c5018f6eb73ed6e5b17ae083aec1d864ae62
S <490723@cognizant.com>
2025-08-08 15:05:39 +0530

    my initial commit

---
+++ src/main/java/edu/in/batch1_sdet_trainer/App.java
@@ -0,0 +1,13 @@
+package edu.in.batch1_sdet_trainer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
